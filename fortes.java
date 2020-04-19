7eb2287b45c5604bfb1bd9b25ebf50574da969a3
Ryoichi Hayashi <hayaryo99@gmail.com>
2020-04-19 14:30:12 +0900

    add new file

---
+++ fortes.java
@@ -0,0 +1,28 @@
+
+public class fortes {
+    static public void main(String arg[]) {
+        
+        th n_th = new th();
+        Thread t = new Thread(n_th);
+        t.start();
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
+        return;
+    }
+    
+
+}
+
+class th implements Runnable {
+    public void gold(){
+        System.out.println("gold");
+    }
+    
+    public void run() {
+        System.out.println("running");
+        gold();
+        while (true) {
+        }
+    }
+}
