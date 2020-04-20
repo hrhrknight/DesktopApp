328e48bdc102be404522f64758d9b783e21ef47e
Ryoichi Hayashi <hayaryo99@gmail.com>
2020-04-20 16:08:19 +0900

    draw timestamp

--- DesktopApp.java
+++ DesktopApp.java
@@ -1,16 +1,18 @@
 import java.awt.Color;
 import java.awt.Image;
+import java.awt.MouseInfo;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Toolkit;
 import java.awt.event.*;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.Calendar;
 import java.awt.Point;
+import java.awt.image.BufferedImage;
 
 import java.awt.Font;
+import java.awt.FontMetrics;
 import javax.swing.JFrame;
-
-
 
 class DesktopApp extends JFrame {
     /**
@@ -21,14 +23,25 @@ class DesktopApp extends JFrame {
     boolean is_closeButton_pressed = false;
     boolean is_movable = false;
     Point mouse_position = new Point();
-    
-    String now = new String();
+    Point rltPnt = new Point();
+    Point charaPnt = new Point();
+    BufferedImage buffer;
+    BufferedImage object;
+    int screenWidth, screenHeight;
+    int charaWidth = 800, charaHeight = 800;
+
+    final int screenMargin = 1;
+
+    String now = new String("This is initialized string.");
 
     DesktopApp() {
+        screenWidth = Toolkit.getDefaultToolkit().getScreenSize().width - screenMargin * 2;
+        screenHeight = Toolkit.getDefaultToolkit().getScreenSize().height - screenMargin * 2;
+
         // set drawable windowsize
-        setSize(Toolkit.getDefaultToolkit().getScreenSize().width, Toolkit.getDefaultToolkit().getScreenSize().height);
+        setSize(screenWidth, screenHeight);
         // set window location
-        setLocation(0, 0);
+        setLocation(screenMargin, screenMargin);
         // disable default decoration
         setUndecorated(true);
         // program exit when the window close
@@ -48,6 +61,9 @@ class DesktopApp extends JFrame {
         this.addMouseListener(mListener);
         this.addMouseMotionListener(mListener);
 
+        buffer = new BufferedImage(screenWidth, screenHeight, BufferedImage.TYPE_INT_ARGB);
+        object = createObjectImage();
+
         foundamental_thread f_runnable = new foundamental_thread();
         Thread f_Thread = new Thread(f_runnable);
         f_Thread.start();
@@ -55,32 +71,47 @@ class DesktopApp extends JFrame {
         // set window visible
         setVisible(true);
     }
-    
 
-
-    public void paint(Graphics g) {
-        if (is_closeButton_pressed) {
-            g.setColor(new Color(255, 0, 0));
-        } else {
-            g.setColor(new Color(0, 0, 255));
-        }
-        g.fillRect(20, 20, 100, 50);
-        int[] polX = { 10, 0, 20 }, polY = { 0, 10, 10 };
-        g.fillPolygon(polX, polY, 3);
-        g.drawImage(MainCharaImg, 0, 0, 800, 800, this);
-        DrawTest(g);
-        g.dispose();
+    private BufferedImage createObjectImage() {
+        BufferedImage img = new BufferedImage(screenWidth, screenHeight, BufferedImage.TYPE_INT_ARGB);
+        return img;
     }
 
-    private void DrawTest(Graphics g) {
-        g.setColor(new Color(0, 255, 0));
+    public void paint(Graphics g) {
+
+        Graphics2D g2 = (Graphics2D) buffer.getGraphics();
+        g2.setBackground(new Color(0, 0, 0, 0));
+        g2.clearRect(0, 0, screenWidth, screenHeight);
+        g2.translate(0, 0);
+        g2.drawImage(object, 0, 0, this);
+
+        ((Graphics2D) g).setBackground(new Color(0, 0, 0, 0));
+        g.clearRect(0, 0, screenWidth, screenHeight);
+        g.drawImage(buffer, screenMargin, screenMargin, this);
+
+    }
+
+    void drawTime() {
+        int timeFrameWidth = (int) (screenWidth * 0.2);
+        int timeFrameHeight = (int) (screenHeight * 0.07);
+        Point timeFramePosition = new Point((int) (screenWidth * 0.75), (int) (screenHeight * 0.9));
+        int timeFrameOutline = 2;
+        Graphics2D g = (Graphics2D) buffer.getGraphics();
+        g.setColor(new Color(0, 0, 255));
+        g.drawRect((int) timeFramePosition.getX() - timeFrameOutline, (int) timeFramePosition.getY() - timeFrameOutline,
+                timeFrameWidth + 2 * timeFrameOutline, timeFrameHeight + 2 * timeFrameOutline);
+        g.setColor(new Color(0x6f, 0xa8, 0xdc, 255));
+        g.fillRect((int) timeFramePosition.getX(), (int) timeFramePosition.getY(), timeFrameWidth, timeFrameHeight);
         Font f = new Font(Font.SANS_SERIF, Font.BOLD, 30);
         g.setFont(f);
-        g.drawString(now, 100, 500);
+        g.setColor(new Color(0, 0, 0));
+        FontMetrics metrics = g.getFontMetrics(f);
+        int timeStampWidth = metrics.stringWidth(now);
+        g.drawString(now, (int) (timeFramePosition.getX() + timeFrameWidth / 2 - timeStampWidth / 2),
+                (int) (timeFramePosition.getY() + timeFrameHeight / 2));
     }
 
     public static void main(String args[]) {
-
         new DesktopApp();
     }
 
@@ -101,11 +132,12 @@ class DesktopApp extends JFrame {
             if ((20 < x && x < 120) && (20 < y && y < 70)) {
                 if (e.getButton() == MouseEvent.BUTTON3) {
                     is_closeButton_pressed = true;
-                    repaint();
                 }
             }
             if (e.getButton() == MouseEvent.BUTTON1) {
                 is_movable = true;
+                rltPnt.x = mouse_position.x - charaPnt.x;
+                rltPnt.y = mouse_position.y - charaPnt.y;
             }
         }
 
@@ -113,21 +145,24 @@ class DesktopApp extends JFrame {
             is_movable = false;
             if (e.getButton() == MouseEvent.BUTTON3) {
                 is_closeButton_pressed = false;
-                repaint();
             }
         }
 
         public void mouseDragged(MouseEvent e) {
-            Point eventLocationOnScreen = new Point();
-            eventLocationOnScreen = e.getLocationOnScreen();
-
-            if (is_movable == true) {
-                e.getComponent().setLocation(eventLocationOnScreen.x - ComponentPoint.x,
-                        eventLocationOnScreen.y - ComponentPoint.y);
+            if (is_movable) {
+                charaPnt.x = mouse_position.x - rltPnt.x;
+                charaPnt.y = mouse_position.y - rltPnt.y;
             }
+            // Point eventLocationOnScreen = new Point();
+            // eventLocationOnScreen = e.getLocationOnScreen();
+            // if (is_movable == true) {
+            // e.getComponent().setLocation(eventLocationOnScreen.x - ComponentPoint.x,
+            // eventLocationOnScreen.y - ComponentPoint.y);
+            // }
         }
 
         public void mouseMoved(MouseEvent e) {
+
         }
     }
 
@@ -135,15 +170,18 @@ class DesktopApp extends JFrame {
 
         public void run() {
             while (true) {
-                Date date = new Date();
-                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-                now = sdf.format(date);
+                mouse_position = MouseInfo.getPointerInfo().getLocation();
+                drawTime();
                 repaint();
+                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+                now = sdf.format(Calendar.getInstance().getTime());
+                try {
+                    Thread.sleep(5l);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
 
 }
-
-
-
